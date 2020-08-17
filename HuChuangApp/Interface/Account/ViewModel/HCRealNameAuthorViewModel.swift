d818aa18034048c593974f43b6653e5bf45f0e54
yintao910816 <ytappledeveloper@163.com>
2020-08-17 10:35:25 +0800

    fix

--- HuChuangApp/Interface/Account/ViewModel/HCRealNameAuthorViewModel.swift
+++ HuChuangApp/Interface/Account/ViewModel/HCRealNameAuthorViewModel.swift
@@ -17,3 +17,20 @@ class HCRealNameAuthorViewModel: BaseViewModel {
         
     }
 }
+
+/**
+ https://ileyun.ivfcn.com/hc-patient-web/api/consumer/realNameAuth
+ realName    是    string    姓名
+ sex    是    int    性别 女 0 ，男 1
+ birthDay    是    string    出生日期 yyyy-MM-dd
+ certificateType    是    string    证件类型 ， 身份证
+ certificateNo    是    string
+ 
+ {"realName":"张三","sex":1,"birthDay":"1991-01-01","certificateType":"身份证",
+ "certificateNo":"429005198907041123"}
+
+ 401    身份失效
+ 402    未实名认证
+ 403    微信未绑定手机号
+ 1111    免费订单
+ */
