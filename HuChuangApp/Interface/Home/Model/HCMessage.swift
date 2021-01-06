ce40e74d3311769a29049308bab68964b70fb154
swift <mlsy.swift@gmail.com>
2021-01-06 20:22:21 +0800

    fix

--- HuChuangApp/Interface/Home/Model/HCMessage.swift
+++ HuChuangApp/Interface/Home/Model/HCMessage.swift
@@ -11,10 +11,10 @@ import Foundation
 class HCMessageItemModel: HJModel {
     var consultId: String = ""
     var code: String = ""
-    var content: String = "答复哈都是废话看哈电视立刻恢复健康哈速度快哈瞌睡的很疯狂啊数据恢复快乐啊速度很快"
-    var date: String = "2020-11-10 11:11"
+    var content: String = ""
+    var date: String = ""
     var headPath: String = ""
-    var name: String = "放大舒服阿斯顿发阿斯顿发阿斯顿发大发"
+    var name: String = ""
     var unReadCount: Int = 0
     var userId: String = ""
 }
